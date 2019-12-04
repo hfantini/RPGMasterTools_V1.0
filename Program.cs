621b5646fd8fdfcd3ab3d45b11e5b20d2eb5e981
Henrique.F <henrique.fantini@certsys.com.br>
2019-12-04 11:06:27 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RPGMasterTools.Source.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGMasterTools
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run( new FrmMain() );
+        }
+    }
+}
